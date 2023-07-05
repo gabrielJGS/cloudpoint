b6ff91607928d69128e6ac53236708847e1301a2
gabrielJGS <gabrielftjose@gmail.com>
2023-07-04 23:02:43 -0300

    basic layout

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const { app, BrowserWindow } = require("electron");
-const path = require('path')
+const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
+    transparent: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
 
   win.loadFile("index.html");
+  win.webContents.openDevTools()
 };
 
 app.whenReady().then(() => {
--- renderer.js
+++ renderer.js
@@ -1,2 +1,6 @@
-const information = document.getElementById("info");
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+
+
+const close = document.getElementById("close");
+close.addEventListener("click", () => {
+  window.close();
+});
